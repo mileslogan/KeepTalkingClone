99874420b0a2c781994f0638dd9a4c16a200c5c3
Mike <mikelyzhang99@gmail.com>
2020-04-12 13:20:53 -0400

    Added TextMeshPro and modified GenerateBomb to spawn batteries, serial#, and indicators, with that scripts are the added scripts for SerialNumber, Respawnself, and indicator behavior to be used with GenerateBomb

---
+++ Assets/Mike/Scripts/IndicatorBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class IndicatorBehavior : MonoBehaviour
+{
+    
+    public int IndicatorIndex; //which index it is referring to, used to respawn self if colliding with another object
+    public TextMeshPro TopSideText;
+    public TextMeshPro FlipSideText;
+    public Light FrontLight;
+    public Light BackLight;
+
+    //set 3 letter word on indicator
+    public void SetText(string text)
+    {
+        TopSideText.text = text;
+        FlipSideText.text = text;
+    }
+
+    //set light on or off-to be replaced with model that has a light on it
+    public void SetLight(bool isOn)
+    {
+        FrontLight.enabled = isOn;
+        BackLight.enabled = isOn;
+    }
+}
---
+++ Assets/SerialNumber.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SerialNumber : MonoBehaviour
+{
+    //beginning text of string on object
+    public string DisplayString = "Serial#\n";
+    public TextMeshPro TopSideText;
+
+    public TextMeshPro FlipSideText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TopSideText.text = DisplayString + FindObjectOfType<GenerateBomb>().SerialN;
+        FlipSideText.text = DisplayString + FindObjectOfType<GenerateBomb>().SerialN;
+    }
+    
+}

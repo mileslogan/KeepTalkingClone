5631745593af1f38e6b10fca348f4255777a25f3
Jordan Resin <jar1063@nyu.edu>
2020-04-14 09:09:14 -0700

    Script Stuff

---
+++ Assets/Modules/Keypad Things/keyScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyScript : MonoBehaviour
+{
+    public ArrayList symbols = new ArrayList();
+
+
+    public Sprite symbol1;
+    public Sprite symbol2;
+    public Sprite symbol3;
+    public Sprite symbol4;
+    public Sprite symbol5;
+    public Sprite symbol6;
+    public Sprite symbol7;
+    public Sprite symbol8;
+    public Sprite symbol9;
+    public Sprite symbol10;
+    public Sprite symbol11;
+    public Sprite symbol12;
+    public Sprite symbol13;
+    public Sprite symbol14;
+    public Sprite symbol15;
+    public Sprite symbol16;
+    public Sprite symbol17;
+    public Sprite symbol18;
+    public Sprite symbol19;
+    public Sprite symbol20;
+    public Sprite symbol21;
+    public Sprite symbol22;
+    public Sprite symbol23;
+    public Sprite symbol24;
+    public Sprite symbol25;
+    public Sprite symbol26;
+    public Sprite symbol27;
+    public Sprite symbol28;
+    public Sprite symbol29;
+    public Sprite symbol30;
+    public Sprite symbol31;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

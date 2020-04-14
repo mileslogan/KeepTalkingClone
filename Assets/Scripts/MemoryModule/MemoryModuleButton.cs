c4f7f367cb4b94c71e9778d4c968e4dc9a808a7d
luna11woolf <yw3264@nyu.edu>
2020-04-14 21:41:44 +0800

    Add the model of Timer and Memory Module in Scene. Add Raycasting and TextMesh.

---
+++ Assets/Scripts/MemoryModule/MemoryModuleButton.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryModuleButton : MonoBehaviour
+{
+    public int buttonIndex;//this number represent the index of the button(from left to right, start with 0)
+}

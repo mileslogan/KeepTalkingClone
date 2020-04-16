b753c4d08736a0f7ad6a7d6c0e575aab861a58b7
Miles Logan <mlr582@nyu.edu>
2020-04-16 07:12:02 -0700

    Further work on simon says script

--- Assets/Scripts/SimonSays/SimonSaysScript.cs
+++ Assets/Scripts/SimonSays/SimonSaysScript.cs
@@ -24,7 +24,7 @@ public class SimonSaysScript : MonoBehaviour
     public GameObject fourthButton;
     public GameObject fifthButton;
 
-    GameObject firstPushButton;
+    public GameObject firstPushButton;
     GameObject secondPushButton;
     GameObject thirdPushButton;
     GameObject fourthPushButton;
@@ -150,5 +150,272 @@ public class SimonSaysScript : MonoBehaviour
                 fifthButton = buttonYellow;
             }
         }
+
+        //Tell what buttons need to be pushed
+        AssignPushButton();
+    }
+
+    void AssignPushButton()
+    {
+        if (serialHasVowel)
+        {
+            switch (strikeCount)
+            {
+                case 0:
+                    //assign first button to be pushed
+                    firstPushButton = NoStrikeVowel(firstButton);
+
+                    //assign second button to be pushed
+                    secondPushButton = NoStrikeVowel(secondButton);
+
+                    //assign third button to be pushed
+                    thirdPushButton = NoStrikeVowel(thirdButton);
+
+                    //if there are four stages, assign fourth button
+                    if (maxStage >= 4)
+                    {
+                        fourthPushButton = NoStrikeVowel(fourthButton);
+                    }
+
+                    //if there are five stages, assign fifth button
+                    if(maxStage == 5)
+                    {
+                        fifthPushButton = NoStrikeVowel(fifthButton);
+                    }
+                    break;
+                case 1:
+                    firstPushButton = OneStrikeVowel(firstButton);
+
+                    secondPushButton = OneStrikeVowel(secondButton);
+
+                    thirdPushButton = OneStrikeVowel(thirdButton);
+
+                    if (maxStage >= 4)
+                    {
+                        fourthPushButton = OneStrikeVowel(fourthButton);
+                    }
+
+                    if (maxStage == 5)
+                    {
+                        fifthPushButton = OneStrikeVowel(fifthButton);
+                    }
+                    break;
+                case 2:
+                    firstPushButton = TwoStrikeVowel(firstButton);
+
+                    secondPushButton = TwoStrikeVowel(secondButton);
+
+                    thirdPushButton = TwoStrikeVowel(thirdButton);
+
+                    if (maxStage >= 4)
+                    {
+                        fourthPushButton = TwoStrikeVowel(fourthButton);
+                    }
+
+                    if (maxStage == 5)
+                    {
+                        fifthPushButton = TwoStrikeVowel(fifthButton);
+                    }
+                    break;
+            }
+        }
+        else
+        {
+            switch (strikeCount)
+            {
+                case 0:
+                    //assign first button to be pushed
+                    firstPushButton = NoStrikeNoVowel(firstButton);
+
+                    //assign second button to be pushed
+                    secondPushButton = NoStrikeNoVowel(secondButton);
+
+                    //assign third button to be pushed
+                    thirdPushButton = NoStrikeNoVowel(thirdButton);
+
+                    //if there are four stages, assign fourth button
+                    if (maxStage >= 4)
+                    {
+                        fourthPushButton = NoStrikeNoVowel(fourthButton);
+                    }
+
+                    //if there are five stages, assign fifth button
+                    if (maxStage == 5)
+                    {
+                        fifthPushButton = NoStrikeNoVowel(fifthButton);
+                    }
+                    break;
+                case 1:
+                    firstPushButton = OneStrikeNoVowel(firstButton);
+
+                    secondPushButton = OneStrikeNoVowel(secondButton);
+
+                    thirdPushButton = OneStrikeNoVowel(thirdButton);
+
+                    if (maxStage >= 4)
+                    {
+                        fourthPushButton = OneStrikeNoVowel(fourthButton);
+                    }
+
+                    if (maxStage == 5)
+                    {
+                        fifthPushButton = OneStrikeNoVowel(fifthButton);
+                    }
+                    break;
+                case 2:
+                    firstPushButton = TwoStrikeNoVowel(firstButton);
+
+                    secondPushButton = TwoStrikeNoVowel(secondButton);
+
+                    thirdPushButton = TwoStrikeNoVowel(thirdButton);
+
+                    if (maxStage >= 4)
+                    {
+                        fourthPushButton = TwoStrikeNoVowel(fourthButton);
+                    }
+
+                    if (maxStage == 5)
+                    {
+                        fifthPushButton = TwoStrikeNoVowel(fifthButton);
+                    }
+                    break;
+            }
+        }
+    }
+
+    //Deem What Buttons Need to be pushed based on the amount of strikes and if the serial code has a vowel
+    GameObject NoStrikeVowel(GameObject flashing)
+    {
+        GameObject push;
+        if (flashing == buttonRed)
+        {
+            push = buttonBlue;
+        }
+        else if (flashing == buttonBlue)
+        {
+            push = buttonRed;
+        }
+        else if (flashing == buttonGreen)
+        {
+            push = buttonYellow;
+        }
+        else
+        {
+            push = buttonGreen;
+        }
+        return push;
+    }
+
+    GameObject OneStrikeVowel(GameObject flashing)
+    {
+        GameObject push;
+        if (flashing == buttonRed)
+        {
+            push = buttonYellow;
+        }
+        else if (flashing == buttonBlue)
+        {
+            push = buttonGreen;
+        }
+        else if (flashing == buttonGreen)
+        {
+            push = buttonBlue;
+        }
+        else
+        {
+            push = buttonRed;
+        }
+        return push;
+    }
+
+    GameObject TwoStrikeVowel(GameObject flashing)
+    {
+        GameObject push;
+        if (flashing == buttonRed)
+        {
+            push = buttonGreen;
+        }
+        else if (flashing == buttonBlue)
+        {
+            push = buttonRed;
+        }
+        else if (flashing == buttonGreen)
+        {
+            push = buttonYellow;
+        }
+        else
+        {
+            push = buttonBlue;
+        }
+        return push;
+    }
+
+    GameObject NoStrikeNoVowel(GameObject flashing)
+    {
+        GameObject push;
+        if (flashing == buttonRed)
+        {
+            push = buttonBlue;
+        }
+        else if (flashing == buttonBlue)
+        {
+            push = buttonYellow;
+        }
+        else if (flashing == buttonGreen)
+        {
+            push = buttonGreen;
+        }
+        else
+        {
+            push = buttonRed;
+        }
+        return push;
+    }
+
+    GameObject OneStrikeNoVowel(GameObject flashing)
+    {
+        GameObject push;
+        if (flashing == buttonRed)
+        {
+            push = buttonRed;
+        }
+        else if (flashing == buttonBlue)
+        {
+            push = buttonBlue;
+        }
+        else if (flashing == buttonGreen)
+        {
+            push = buttonYellow;
+        }
+        else
+        {
+            push = buttonGreen;
+        }
+        return push;
+    }
+
+    GameObject TwoStrikeNoVowel(GameObject flashing)
+    {
+        GameObject push;
+        if (flashing == buttonRed)
+        {
+            push = buttonYellow;
+        }
+        else if (flashing == buttonBlue)
+        {
+            push = buttonGreen;
+        }
+        else if (flashing == buttonGreen)
+        {
+            push = buttonBlue;
+        }
+        else
+        {
+            push = buttonRed;
+        }
+        return push;
     }
 }
+
+
+

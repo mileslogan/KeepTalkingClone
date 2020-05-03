b353b600a795d48f16c5c7879f38715bbca6f311
Miles Logan <mlr582@nyu.edu>
2020-05-03 00:05:48 -0700

    Start Scene Finished

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -1,36 +1,107 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class StartScreen : MonoBehaviour
 {
     public static int numOfModules;
     public static int timeForDefusal;
 
-    public GenerateBomb bombScript;
+    public Animator binderAnim;
+    public Collider binderCollider;
+
+    public TextMeshPro moduleNum;
+    public TextMeshPro timerNum;
+
+    int minutes;
+    int seconds;
+
+    string minuteString;
+    string secondString;
+
+    public AudioSource clickSource;
+    public AudioClip clickClip;
 
     void Awake()
     {
-        DontDestroyOnLoad(this);
+        //DontDestroyOnLoad(this);
     }
 
     void Start()
     {
-        bombScript = FindObjectOfType<GenerateBomb>();
+        numOfModules = 3;
+        timeForDefusal = 300;
+
     }
     // Update is called once per frame
     void Update()
     {
-        if(bombScript == null)
+        
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit))
+            if(binderAnim.GetBool("Opened") == false)
             {
-
+                if (hit.collider.tag == "Binder" && Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    binderCollider.enabled = false;
+                    binderAnim.SetBool("Opened", true);
+                }
             }
+            else
+            {
+                if (hit.collider.tag == "StartModMinus" && Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    clickSource.PlayOneShot(clickClip);
+                    MinusModule();
+                }
+                else if(hit.collider.tag == "StartModPlus" && Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    clickSource.PlayOneShot(clickClip);
+                    PlusModule();
+                }
+                else if(hit.collider.tag == "StartTimeMinus" && Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    clickSource.PlayOneShot(clickClip);
+                    MinusTime();
+                }
+                else if(hit.collider.tag == "StartTimePlus" && Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    clickSource.PlayOneShot(clickClip);
+                    PlusTime();
+                }
+                else if(hit.collider.tag == "StartStart" && Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    clickSource.PlayOneShot(clickClip);
+                    StartGame();
+                }
+            }
+            
         }
+
+        minutes = (int)timeForDefusal / 60;
+        seconds = (int)timeForDefusal % 60;
+        moduleNum.text = "" + numOfModules;
+        if(minutes < 10)
+        {
+            minuteString = "0" + minutes;
+        }
+        else
+        {
+            minuteString = "" + minutes;
+        }
+        if (seconds < 10)
+        {
+            secondString = "0" + seconds;
+        }
+        else
+        {
+            secondString = "" + seconds;
+        }
+        timerNum.text = "" + minuteString + ":" + secondString;
     }
 
 
@@ -52,17 +123,22 @@ public class StartScreen : MonoBehaviour
 
     void PlusTime()
     {
-        if(timeForDefusal < 20)
+        if(timeForDefusal < 600)
         {
-            timeForDefusal++;
+            timeForDefusal += 30;
         }
     }
 
     void MinusTime()
     {
-        if(timeForDefusal > 1)
+        if(timeForDefusal > 30)
         {
-            timeForDefusal--;
+            timeForDefusal -= 30;
         }
+    }
+
+    void StartGame()
+    {
+
     }
 }

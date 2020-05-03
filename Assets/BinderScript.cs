f4b935639c23cc0552df1db557dba23f3e96b221
Jordan Resin <jar1063@nyu.edu>
2020-05-02 20:35:44 -0700

    binder animation

---
+++ Assets/BinderScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class BinderScript : MonoBehaviour
+{
+    public GameObject binder;
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit binderHit;
+            if (Physics.Raycast(ray, out binderHit)){
+                if (binderHit.transform.gameObject == binder)
+                {
+
+                    anim.SetTrigger("Open");
+                }
+
+                }
+
+            }
+    }
+    }
